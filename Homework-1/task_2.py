1b147ad30d20ec3dfde7d2608ff8f7a57894f752
gobeja16 <gobejishvilimisho@gmail.com>
2024-02-25 17:23:25 +0400

    Finished first homework

---
+++ Homework-1/task_2.py
@@ -0,0 +1,8 @@
+
+# 2. პროგრამამ უნდა შეგაყვანინოთ სახელი და დაბადების წელი. ეკრანზე უნდა გამოვიდეს, გამარჯობა, „თქვენს მიერ შეყვანილი სახელი“, თქვენ ხართ „თქვენი წლოვანება“ წლის.
+
+name = input("Enter your name ")
+yearOfBirth = int(input("Enter your year of birth "))
+
+print()
+print(f"Hello {name}, You are {2024 - yearOfBirth} years old. \n")
