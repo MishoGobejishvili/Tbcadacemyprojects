cc2b3fb210eb165855271c09c8627970020357f5
Mikheil Gobejishvili <72811348+MishoGobejishvili@users.noreply.github.com>
2024-03-05 00:56:36 +0400

    Uploaded homework 3

---
+++ Homework-3/task_1.py
@@ -0,0 +1,8 @@
+
+# 1. დაწერეთ პროგრამა რომელიც მიიღებს ორ რიცხვს (x,y) და დაბეჭდავს რიცხვს რომელიც მიიღება x-ის y ხარისხში აყვანით.
+
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))
+
+print(f"{x} raised to the power of {y} Equals {pow(x, y)}")
+
