3af6bcdedabd58893670e93ca2ac97b0dedd2d09
Mikheil Gobejishvili <72811348+MishoGobejishvili@users.noreply.github.com>
2024-03-05 00:23:21 +0400

    Deleted Homework 1 from Homework 3 branch

--- Homework-1/task_2.py
+++
@@ -1,8 +0,0 @@
-
-# 2. პროგრამამ უნდა შეგაყვანინოთ სახელი და დაბადების წელი. ეკრანზე უნდა გამოვიდეს, გამარჯობა, „თქვენს მიერ შეყვანილი სახელი“, თქვენ ხართ „თქვენი წლოვანება“ წლის.
-
-name = input("Enter your name ")
-yearOfBirth = int(input("Enter your year of birth "))
-
-print()
-print(f"Hello {name}, You are {2024 - yearOfBirth} years old.")
